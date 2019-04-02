20077be101efff2e70c97dd2a05b7f37cb886726
menendezisma <mauriciomenendez01@gmail.com>
2019-04-02 16:55:40 -0600

    version final de la interfaz que se utilizara en el programa, para el registro de estudiantes

---
+++ src/Gui/MainVentanas.java
@@ -0,0 +1,12 @@
+
+package Gui;
+
+
+public class MainVentanas {
+    public static void main(String [] args){
+        registro vent=new registro();
+        vent.setLocationRelativeTo(null);
+        vent.setVisible(true);
+        vent.setTitle("ventana principal");
+    }
+}
